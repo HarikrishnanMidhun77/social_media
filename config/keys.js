45aabf94a7944e39632d8a3ba17c6e369872aa6d
hari <inviteforuou.com@gmail.com>
2019-11-04 23:12:32 +0530

    before passport implementation

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://harikrishnan_midhun:Hariml7736@ds241968.mlab.com:41968/sho_app"
+    "mongodb://harikrishnan_midhun:Hariml7736@ds241968.mlab.com:41968/sho_app",
+  secretkey: "hari"
 };
--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
 
 const users = require("./routes/apis/users");
 const posts = require("./routes/apis/posts");
 const profile = require("./routes/apis/profile");
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(db)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 app.get("/", (req, res) => res.send("Hello world by Hari"));
-const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
